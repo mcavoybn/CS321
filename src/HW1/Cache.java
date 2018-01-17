a1edd84db43f0d5776f62fac4df4afec9e2bffc1
Ben McAvoy <mcavoybn@gmail.com>
2018-01-17 10:46:58 -0700

    eeeee

--- src/HW1/Cache.java
+++ src/HW1/Cache.java
@@ -2,52 +2,44 @@ package HW1;
 
 import java.util.LinkedList;
 
-public class Cache {
-	
-	public int l1HitCount;
-	public int l2HitCount;
-	
-	public int l1RefCount;
-	public int l2RefCount;
+public class Cache <E> {
 	
 	private int l1maxSize;
-	private int l2maxSize;
-	
+	private int l2maxSize;	
 	public boolean l2enabled;
+	public int l1HitCount;
+	public int l2HitCount;
+	public int l1RefCount;
+	public int l2RefCount;	
+	private LinkedList<E> l1;
+	private LinkedList<E> l2;
 	
-	private LinkedList l1;
-	private LinkedList l2;
-	
-	public <E> Cache(int l1size, int l2size, boolean l2enable) {
-		l1HitCount = 0;
-		l2HitCount = 0;
-		
-		l1RefCount = 0;
-		l2RefCount = 0;
-		
+	public Cache(int l1size, int l2size, boolean l2enable) {
 		l1maxSize = l1size;
 		l2maxSize = l2size;
-		
 		l2enabled = l2enable;
+		l1HitCount = 0;
+		l2HitCount = 0;
+		l1RefCount = 0;
+		l2RefCount = 0;
 		l1 = new LinkedList<E>();
 		if(l2enabled) l2 = new LinkedList<E>();
 	}
-	
-	
-	public <E> void addObject(E object) {
+
+	public void addObject(E object) {
 		l1.addFirst(object);
 		if(l2enabled) l2.addFirst(object);
 		if(l1.size() >= l1maxSize) l1.removeLast();
 		if(l2enabled && l2.size() >= l2maxSize) l2.removeLast();
 	}
 
-	public <E> E getObject(E object) {
+	public E getObject(E object) {
 		removeObject(object);
 		addObject(object);
 		return (E)l1.get(0);
 	}
 
-	public <E> void removeObject(E object) {
+	public void removeObject(E object) {
 		l1RefCount++;
 		if(l1.indexOf(object) != -1) {
 			l1HitCount++;
@@ -62,7 +54,7 @@ public class Cache {
 		}
 	}
 	
-	public <E> void clearCache(E object){
+	public void clearCache(E object){
 		l1.clear();
 		if(l2enabled) l2.clear();
 	}
--- src/HW1/Test.java
+++ src/HW1/Test.java
@@ -8,21 +8,21 @@ public class Test {
 
 	public static void main(String[] args){
 		if(args[0].equals("1")) {
-			Cache cache = new Cache( Integer.parseInt(args[1]), 0, false);
+			Cache<String> cache = new Cache<String>( Integer.parseInt(args[1]), 0, false);
 			System.out.println("First level cache with " + args[1] + " entries has been created");
-			File f = new File("./" + args[2]); //this needs to be fixed		
+			File f = new File(System.getProperty("user.dir") + "/src/HW1/" + args[2]); //this needs to be fixed		
 			getCacheStats(cache, f);
 		}
 		if(args[0].equals("2")) {
-			Cache cache = new Cache( Integer.parseInt(args[1]), Integer.parseInt(args[2]), true);
+			Cache<String> cache = new Cache<String>( Integer.parseInt(args[1]), Integer.parseInt(args[2]), true);
 			System.out.println("First level cache with " + args[1] + " entries has been created");
 			System.out.println("Second level cache with " + args[2] + " entries has been created");
-			File f = new File(System.getProperty("user.dir") + args[3]); //this needs to be fixed		
+			File f = new File(System.getProperty("user.dir") + "/src/HW1/" + args[3]); //this needs to be fixed		
 			getCacheStats(cache, f);
 		}			
 	}	
 	
-	public static void getCacheStats(Cache c, File f) {
+	public static void getCacheStats(Cache<String> c, File f) {
 		Scanner in;
 		try {
 			in = new Scanner(f);
@@ -31,7 +31,7 @@ public class Test {
 			while(in.hasNext()) {
 				c.getObject(in.next());
 				counter++;
-				if(counter%10000==0 && counter<10000000) System.out.print('.');
+				if(counter%50000==0 && counter<1000000) System.out.print('.');
 			}
 			System.out.println(' ');
 			in.close();
@@ -41,15 +41,15 @@ public class Test {
 		
 		int totalReferences = c.l1RefCount + c.l2RefCount;
 		int totalHits = c.l1HitCount + c.l2HitCount;
-		double globalHitRatio = ((double)totalHits/(double)c.l1RefCount);
-		double l1HitRatio = ((double)c.l1HitCount/(double)c.l1RefCount);
+		double globalHitRatio = ((double)totalHits / (double)c.l1RefCount);
+		double l1HitRatio = ((double)c.l1HitCount / (double)c.l1RefCount);
 		System.out.println("Total number of references: " +  totalReferences);
 		System.out.println("Total number of cache hits: " + totalHits);
 		if(c.l2enabled) System.out.println("The global hit ratio: " + globalHitRatio);
 		System.out.println("Number of 1st-level cache hits: " + c.l1RefCount);
 		System.out.println("1st level cache hit ratio: " + l1HitRatio);
 		if(c.l2enabled) {
-			double l2HitRatio = ((double)c.l2HitCount/(double)c.l2RefCount);
+			double l2HitRatio = ((double)c.l2HitCount / (double)c.l2RefCount);
 			System.out.println("Number of 2nd-level cache hits: " + c.l2RefCount);
 			System.out.println("2nd level cache hit ratio: " + l2HitRatio);
 		}
